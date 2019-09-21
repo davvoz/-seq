676bdf329d531ab20a7d0994d0cab6e1338d4f90
davvoz <dav.baz@alice.it>
2019-09-21 20:24:32 +0200

    5 files modified, 3 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { SoundService } from './services/sound.service';
 import { TimerService } from './services/timer.service';
 import { TimerMonitorComponent } from './timer-monitor/timer-monitor.component';
 import { SpeedControlComponent } from './speed-control/speed-control.component';
+import { PianoRollComponent } from './piano-roll/piano-roll.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, TimerMonitorComponent, SpeedControlComponent ],
+  declarations: [ AppComponent, TimerMonitorComponent, SpeedControlComponent, PianoRollComponent ],
   bootstrap:    [ AppComponent ],
   providers: [SoundService, TimerService]
 })
